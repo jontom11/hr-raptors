231210ef596c5595d7d3d93da27aa9ff80e66e41
alexitaylor <alexi@bluesunrise.com>
2017-05-26 14:42:44 -0700

    Implemented actions w/ axios, connecting to backend

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,3 +1,18 @@
-/**
- * Created by alexi on 5/26/17.
- */
+import axios from 'axios';
+
+module.exports = {
+  fetchUser: () => {
+    return function(dispatch) {
+      dispatch({type: 'FETCH_USER'});
+
+      axios.get('http://127.0.0.1:3000/api/users')
+      .then((response) => {
+        console.log(response.data);
+        dispatch({type: 'FETCH_USER_FULFILLED', payload: response.data});
+      })
+      .catch((err) => {
+        dispatch({type: 'FETCH_USER_REJECTED', payload: err});
+      });
+    };
+  },
+};
