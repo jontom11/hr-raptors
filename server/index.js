8e15009acb1e367d7d08402b01b0c513db6a1d0c
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-05-27 11:39:22 -0700

    moving webpack in package.json out of dev-dependencies to dependencies

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ const db = require('../db');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port $(PORT)!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
