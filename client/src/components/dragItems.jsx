a43eb51687bd9e16fe1c559f800a97441d667de2
alexitaylor <alexi@bluesunrise.com>
2017-05-30 15:07:38 -0700

    Code clean up, materialize components
    No skewer case in jsx.
    Make sure to having closing tags in jsx.
    “className” instead of “class”.
    We should expand on more than one icon (currently just have add). So we need to figure out how select from hundred or so icons or provide the most common icons (top ten).
    Dropdown involves jQuery so the HTML code will not work. We will get to javascript components after MVP.
    Added multiple options for rows/columns. We might want to make this more dynamic.
     We already have material Icons link in our header, it lives in server/views/head.ejs.

--- client/src/components/dragItems.jsx
+++ client/src/components/dragItems.jsx
@@ -1,165 +1,180 @@
 import React from 'React';
 
 module.exports = {
-  button: <a class="waves-effect waves-light btn-large">Button</a>,
-  header-footer:
-  <footer class="page-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">Footer Content</h5>
-          <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-        </div>
-        <div class="col l4 offset-l2 s12">
-          <h5 class="white-text">Links</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-          </ul>
+  button: <a className="waves-effect waves-light btn-large">Button</a>,
+  footer:
+    <footer className="page-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col l6 s12">
+            <h5 className="white-text">Footer Content</h5>
+            <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+          </div>
+          <div className="col l4 offset-l2 s12">
+            <h5 className="white-text">Links</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-      © 2014 Copyright Text
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      <div className="footer-copyright">
+        <div className="container">
+          © 2014 Copyright Text
+          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
       </div>
-    </div>
-  </footer>,
+    </footer>,
   card:
-  <div class="row">
-    <div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
+    <div className="row">
+      <div className="col s12 m6">
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Card Title</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div className="card-action">
+            <a href="#">This is a link</a>
+            <a href="#">This is a link</a>
+          </div>
         </div>
       </div>
-    </div>
-  </div>,
-  form,
-  <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+    </div>,
+  form:
+    <div className="row">
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s6">
+            <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
             <label for="first_name">First Name</label>
           </div>
-          <div class="input-field col s6">
-            <input id="last_name" type="text" class="validate">
-              <label for="last_name">Last Name</label>
+          <div className="input-field col s6">
+            <input id="last_name" type="text" className="validate" />
+            <label for="last_name">Last Name</label>
           </div>
         </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input disabled value="I am not editable" id="disabled" type="text" class="validate">
+        <div className="row">
+          <div className="input-field col s12">
+            <input disabled value="I am not editable" id="disabled" type="text" className="validate" />
             <label for="disabled">Disabled</label>
           </div>
         </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input id="password" type="password" class="validate">
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="password" type="password" className="validate" />
             <label for="password">Password</label>
           </div>
         </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input id="email" type="email" class="validate">
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="email" type="email" className="validate" />
             <label for="email">Email</label>
           </div>
         </div>
-        <div class="row">
-          <div class="col s12">
+        <div className="row">
+          <div className="col s12">
             This is an inline input field:
-            <div class="input-field inline">
-              <input id="email" type="email" class="validate">
+            <div className="input-field inline">
+              <input id="email" type="email" className="validate" />
               <label for="email" data-error="wrong" data-success="right">Email</label>
             </div>
           </div>
         </div>
       </form>
     </div>,
-    icon:
-    // To be able to use these icons, you must include this line in the <head>portion of your HTML code
-    // <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <i class="material-icons">add</i>,
-    navbar:
+  icon: <i className="material-icons">add</i>,
+  container:
+    <div className="container">
+    </div>,
+  rowCol1:
+    <div className="row">
+      <div className="col s12">1</div>
+    </div>,
+  rowCol2:
+    <div className="row">
+      <div className="col s6">1</div>
+      <div className="col s6">2</div>
+    </div>,
+  rowCol3:
+    <div className="row">
+      <div className="col s4">1</div>
+      <div className="col s4">2</div>
+      <div className="col s4">3</div>
+    </div>,
+  rowCol4:
+    <div className="row">
+      <div className="col s3">1</div>
+      <div className="col s3">2</div>
+      <div className="col s3">3</div>
+      <div className="col s3">4</div>
+    </div>,
+  rowCol12:
+    <div className="row">
+      <div className="col s1">1</div>
+      <div className="col s1">2</div>
+      <div className="col s1">3</div>
+      <div className="col s1">4</div>
+      <div className="col s1">5</div>
+      <div className="col s1">6</div>
+      <div className="col s1">7</div>
+      <div className="col s1">8</div>
+      <div className="col s1">9</div>
+      <div className="col s1">10</div>
+      <div className="col s1">11</div>
+      <div className="col s1">12</div>
+    </div>,
+  navbar:
     <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">Logo</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="sass.html">Sass</a></li>
           <li><a href="badges.html">Components</a></li>
           <li><a href="collapsible.html">JavaScript</a></li>
         </ul>
       </div>
     </nav>,
-    dropdown:
-    // <!-- Dropdown Trigger -->
-    // <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
-    // <!-- Dropdown Structure -->
-    <ul id='dropdown1' class='dropdown-content'>
-      <li><a href="#!">one</a></li>
-      <li><a href="#!">two</a></li>
-      <li class="divider"></li>
-      <li><a href="#!">three</a></li>
-      <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-      <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
-    </ul>,
-    grid:
-    <body>
-      <div class="container">
-        <!-- Page Content goes here -->
-      </div>
-    </body>,
-    rows:
-    <div class="row">
-      <div class="col s12">This div is 12-columns wide</div>
-      <div class="col s6">This div is 6-columns wide</div>
-      <div class="col s6">This div is 6-columns wide</div>
-    </div>,
-    table:
+  table:
     <table>
       <thead>
-        <tr>
-          <th>Column A</th>
-          <th>Column B</th>
-          <th>Column C</th>
-        </tr>
+      <tr>
+        <th>Column A</th>
+        <th>Column B</th>
+        <th>Column C</th>
+      </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Data</td>
-          <td>Data</td>
-          <td>Data</td>
-        </tr>
-        <tr>
-          <td>Data</td>
-          <td>Data</td>
-          <td>Data</td>
-        </tr>
-        <tr>
-          <td>Data</td>
-          <td>Data</td>
-          <td>Data</td>
-        </tr>
+      <tr>
+        <td>Data</td>
+        <td>Data</td>
+        <td>Data</td>
+      </tr>
+      <tr>
+        <td>Data</td>
+        <td>Data</td>
+        <td>Data</td>
+      </tr>
+      <tr>
+        <td>Data</td>
+        <td>Data</td>
+        <td>Data</td>
+      </tr>
       </tbody>
     </table>,
-    image:
-    <img class="responsive-img" src="cool_pic.jpg">,
-    embedded-video:
-    <div class="video-container">
+  image:
+    <div>
+      <img className="responsive-img" src="cool_pic.jpg" />
+    </div>,
+  embeddedVideo:
+    <div className="video-container">
       <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
     </div>,
-    responsive-video:
-    <video class="responsive-video" controls>
-      <source src="movie.mp4" type="video/mp4">
+  responsiveVideo:
+    <video className="responsive-video" controls>
+      <source src="movie.mp4" type="video/mp4" />
     </video>,
 };
