4bcabffb04bba56ac9243366cc85983f3170d5bf
Brandon <blbrown13@gmail.com>
2017-05-30 14:09:40 -0700

    Add CSS code for main components requried for MVP. Added comments to several components.

--- client/src/components/dragItems.jsx
+++ client/src/components/dragItems.jsx
@@ -1,8 +1,8 @@
-import React from 'React'; 
+import React from 'React';
 
 module.exports = {
   button: <a class="waves-effect waves-light btn-large">Button</a>,
-  headers: 
+  header-footer:
   <footer class="page-footer">
     <div class="container">
       <div class="row">
@@ -27,6 +27,139 @@ module.exports = {
       <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
       </div>
     </div>
-  </footer>
+  </footer>,
+  card:
+  <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>,
+  form,
+  <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+            <label for="first_name">First Name</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="last_name" type="text" class="validate">
+              <label for="last_name">Last Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input disabled value="I am not editable" id="disabled" type="text" class="validate">
+            <label for="disabled">Disabled</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="password" type="password" class="validate">
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="email" type="email" class="validate">
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12">
+            This is an inline input field:
+            <div class="input-field inline">
+              <input id="email" type="email" class="validate">
+              <label for="email" data-error="wrong" data-success="right">Email</label>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>,
+    icon:
+    // To be able to use these icons, you must include this line in the <head>portion of your HTML code
+    // <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <i class="material-icons">add</i>,
+    navbar:
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>,
+    dropdown:
+    // <!-- Dropdown Trigger -->
+    // <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
+    // <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+      <li><a href="#!">one</a></li>
+      <li><a href="#!">two</a></li>
+      <li class="divider"></li>
+      <li><a href="#!">three</a></li>
+      <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+      <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+    </ul>,
+    grid:
+    <body>
+      <div class="container">
+        <!-- Page Content goes here -->
+      </div>
+    </body>,
+    rows:
+    <div class="row">
+      <div class="col s12">This div is 12-columns wide</div>
+      <div class="col s6">This div is 6-columns wide</div>
+      <div class="col s6">This div is 6-columns wide</div>
+    </div>,
+    table:
+    <table>
+      <thead>
+        <tr>
+          <th>Column A</th>
+          <th>Column B</th>
+          <th>Column C</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+        </tr>
+        <tr>
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+        </tr>
+        <tr>
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+        </tr>
+      </tbody>
+    </table>,
+    image:
+    <img class="responsive-img" src="cool_pic.jpg">,
+    embedded-video:
+    <div class="video-container">
+      <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>,
+    responsive-video:
+    <video class="responsive-video" controls>
+      <source src="movie.mp4" type="video/mp4">
+    </video>,
 };
-
