2b25c493b8f53d7292c47ceed8008a42cd61ca20
Nobodyz710 <colbyduhon@Colbys-MacBook-Air.local>
2017-06-02 15:40:48 -0700

    (Feat) Added beautifyJS to render HTML code in nicer format

--- client/src/components/Code/code.js
+++ client/src/components/Code/code.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
+
 import { connect } from "react-redux"
 import { fetchCode } from "../../actions/codeActions"
+var HtmlToReactParser = require('html-to-react').Parser;
 //
+let beautify_html = require('js-beautify').html;
+let beautify = require('js-beautify');
 
 @connect((store) => {
   return {
@@ -25,29 +29,30 @@ class Code extends React.Component {
 
   render() {
     const { code } = this.props;
-
-    // if (!code.length) {
-    //   return (
-    //     <article className="center">
-    //       <div>
-    //          We gonna need some code here
-    //       </div>
-    //     </article>
-    //   );
-    // }
+    var codeString ='';
     var count = 0;
     const mappedCode = code.map((code, key) =>
       <div key={key} className="codepart">
+        {console.log(code.text)}
         {count++} {ReactDOMServer.renderToStaticMarkup(<div>{code.text}</div>)}
       </div>
-    );
+    )
 
+      mappedCode.forEach((obj, index) =>
+        codeString+=obj.props.children[3],
+      )
+   
+        var perfstring = ('<div>' + codeString +'</div>');
+        var hardCode = "<div><div className='display-linebreak'>Bob</div></div>" ;
+        var pretty = beautify_html(perfstring, {'eol':'\n','end-with-newline':true, 'wrap-attributes':'force', 'extra-liners':'a'});
+          console.log(pretty);
+            console.log(hardCode === pretty);
     return (
       <article className="center-content">
         <h1>Materialize Studio</h1>
         <h2>Your project code goes here</h2>
-        <div className="scrollbar" id="style-1">
-          {mappedCode} 
+        <div className="scrollbar display-linebreak" id="style-1">
+          {pretty} 
         </div>
       </article>
     )
