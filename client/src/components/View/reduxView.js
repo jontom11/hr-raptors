6e2ae521db6b0000846a4327e5519526ae291be0
alexitaylor <alexi@bluesunrise.com>
2017-06-03 12:35:11 -0700

    Add second drop location, but need to make it independent to first

--- client/src/components/View/reduxView.js
+++ client/src/components/View/reduxView.js
@@ -1,20 +1,17 @@
 import React from "react"
 import { connect } from "react-redux"
-import { fetchUser } from "../../actions/userActions"
-import { addCode, clearCode } from "../../actions/codeActions"
+import { addCode } from "../../actions/codeActions"
 import dragItems from '../dragItems.js'
 
 
 const styles = {
   bottomUp: {
-    marginBottom: '-2%',
+    margin: '-2% 0',
   },
 };
 
 @connect((store) => {
   return {
-    user: store.user.user,
-    userFetched: store.user.fetched,
     code: store.code.code,
   };
 })
@@ -24,32 +21,19 @@ class reduxView extends React.Component {
     this.state = {};
   }
 
-  componentWillMount() {
-    this.props.dispatch(fetchUser());
-  }
-
   componentWillReceiveProps(newProps) {
     if (newProps.newComponentID !== this.props.newComponentID) {
       this.props.dispatch(addCode(newProps.newComponentID, dragItems[newProps.newComponentName]));
     }
   }
 
-  clearCodeClick() {
-    this.props.dispatch(clearCode());
-  }
-
-
   render() {
-    const { user, code } = this.props;
+    const { code } = this.props;
     const mappedCode = code.map((code, key) => <li key={key}>{code.text}</li>);
 
     return (
       <div style={styles.bottomUp}>
-        <h1>{user.name} <small>{user.age}</small></h1>
-        <button onClick={this.clearCodeClick.bind(this)}>clear code</button>
         <ul>{mappedCode}</ul>
-
-
       </div>
     );
   }
--- client/src/components/View/view.js
+++ client/src/components/View/view.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { connect } from "react-redux"
+import { fetchUser } from "../../actions/userActions"
+import { clearCode } from "../../actions/codeActions"
 
 import ReduxView from './reduxView';
 import ComponentView from './componentView';
 
+@connect((store) => {
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    code: store.code.code,
+  };
+})
 class View extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +21,14 @@ class View extends React.Component {
       currentItemID: 0,
     };
     this.AddedComponent = this.AddedComponent.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.dispatch(fetchUser());
+  }
+
+  clearCodeClick() {
+    this.props.dispatch(clearCode());
   }
 
   AddedComponent(droppedInItem) {
@@ -22,10 +40,13 @@ class View extends React.Component {
   }
 
   render() {
+    const { user } = this.props;
 
     return (
       <article className="center-content">
-
+        <h1>{user.name} <small>{user.age}</small></h1>
+        <button onClick={this.clearCodeClick.bind(this)}>clear code</button>
+        <ComponentView added={this.AddedComponent.bind(this)}/>
         <ReduxView newComponentName={this.state.currentItem} newComponentID={this.state.currentItemID}/>
         <ComponentView added={this.AddedComponent.bind(this)}/>
 
