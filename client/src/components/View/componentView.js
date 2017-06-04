578e6540395ddcef0fdb61428bcaafd726e3fba7
Nobodyz710 <colbyduhon@Colbys-MacBook-Air.local>
2017-06-03 18:33:05 -0700

    Fix for rebase

---
+++ client/src/components/View/componentView.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { DropTarget } from 'react-dnd';
+import { ItemTypes } from './constants';
+
+const squareTarget = {
+  drop(props, monitor) {
+    const item = monitor.getItem();
+    props.added(item.component);
+    return item;
+  }
+};
+
+let collect = function(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+};
+
+
+class ComponentView extends React.Component {
+  renderOverlay(color) {
+    return (
+      <div>
+       <div style={{
+         height: '20px',
+         width: '100%',
+         backgroundColor: color,
+       }} />
+       </div>
+    );
+  }
+
+  render() {
+    const { connectDropTarget, isOver } = this.props;
+    return connectDropTarget(
+      <div>
+        {isOver && this.renderOverlay('red')}
+        {!isOver && this.renderOverlay('blue')}
+      </div>
+    );
+  }
+}
+export default DropTarget(ItemTypes.COMPONENT, squareTarget, collect)(ComponentView, squareTarget);
