345c1c96ff373ca9c6ee700415b13fb9401b45ae
alexitaylor <alexi@bluesunrise.com>
2017-06-05 12:32:12 -0700

    Implemented queue data structure
    Needed for Traverse Breadth-First

--- client/src/dataStructure/tree.js
+++ client/src/dataStructure/tree.js
@@ -1,13 +1,15 @@
+import queue from './queue';
+
 var Tree = function(component, dropComponent) {
   var node = new Node(component, dropComponent);
   this._root = node;
-}
+};
 
-//  traverses a tree with depth-first search.
+//  traverses a tree with depth-first search
 Tree.prototype.traverseDF = function(callback) {
 
   // Immediately invoking function, recursing
-  (function recurse(currentNode){
+  (function recurse(currentNode) {
     for (var i = 0; i < currentNode.children.length; i++) {
       recurse(currentNode.children[i]);
     }
@@ -15,11 +17,27 @@ Tree.prototype.traverseDF = function(callback) {
   })(this._root);
 };
 
-var Node = function(component, dropComponent){
+// traverses a tree with  breadth-first search
+Tree.prototype.traverseBF = function(callback) {
+  var queue = new Queue();
+
+  queue.enqueue(this._root);
+  currentTree = queue.dequeue();
+
+  while (currentTree) {
+    for (var i  = 0; i < currentTree.children.lenght; i++) {
+      queue.enqueue(currentTree.children[i]);
+    }
+    callback(currentTree);
+    currentTree = queue.dequeue();
+  }
+};
+
+var Node = function(component, dropComponent) {
   this.component = component;
   this.dropComponent = dropComponent;
   this.parent = null;
   this.children = [];
-}
+};
 
 
