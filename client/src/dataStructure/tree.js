d2b51391daff037bc11b34eba9853be246ac7081
alexitaylor <alexi@bluesunrise.com>
2017-06-05 12:12:10 -0700

    Added traverse depth-first function

--- client/src/dataStructure/tree.js
+++ client/src/dataStructure/tree.js
@@ -1,9 +1,21 @@
-function Tree(component, dropComponent) {
-  var node  = new Node(component, dropComponent);
+var Tree = function(component, dropComponent) {
+  var node = new Node(component, dropComponent);
   this._root = node;
 }
 
-function Node(component, dropComponent){
+//  traverses a tree with depth-first search.
+Tree.prototype.traverseDF = function(callback) {
+
+  // Immediately invoking function, recursing
+  (function recurse(currentNode){
+    for (var i = 0; i < currentNode.children.length; i++) {
+      recurse(currentNode.children[i]);
+    }
+    callback(currentNode);
+  })(this._root);
+};
+
+var Node = function(component, dropComponent){
   this.component = component;
   this.dropComponent = dropComponent;
   this.parent = null;
