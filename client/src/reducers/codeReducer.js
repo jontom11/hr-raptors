2bcdff65af704d9603a1905aa055def3ea15f0f9
blbrown13 <blbrown13@gmail.com>
2017-06-09 16:02:55 -0700

    Load user projects functionality

--- client/src/reducers/codeReducer.js
+++ client/src/reducers/codeReducer.js
@@ -12,6 +12,8 @@ export default function reducer(state = {
   tree: {},
   saving: false,
   saved: false,
+  loading: false,
+  loaded: false,
 }, action) {
 
   switch (action.type) {
@@ -56,14 +58,23 @@ export default function reducer(state = {
   case 'UPDATE_TREE': {
     return Object.assign({}, state, { tree: action.payload.tree });
   }
-  case 'SAVE_TREE': {
+  case 'SAVE_PROJECT': {
     return Object.assign({}, state, {saving: true});
   }
-  case 'SAVE_TREE_REJECTED': {
+  case 'SAVE_PROJECT_REJECTED': {
     return Object.assign({}, state, {saving: false, error: action.payload});
   }
-  case 'SAVE_TREE_FULFILLED': {
+  case 'SAVE_PROJECT_FULFILLED': {
     return Object.assign({}, state, {saving: false, saved: true});
+  }
+  case 'LOAD_PROJECTS': {
+    return Object.assign({}, state, {loading: true});
+  }
+  case 'LOAD_PROJECTS_REJECTED': {
+    return Object.assign({}, state, {loading: false, error: action.payload});
+  }
+  case 'LOAD_PROJECTS_FULFILLED': {
+    return Object.assign({}, state, {loading: false, loaded: true});
   }
   }
 
--- server/routes/postgres.js
+++ server/routes/postgres.js
@@ -3,14 +3,16 @@ const express = require('express');
 const router = express.Router();
 const util = require('util');
 
-
 router.route('/tree')
   .post((req, res) => {
-    // save to postgres db here
-
+    // save tree data to postgres db here
     console.log(util.inspect(req.body.codeTree, { showHidden: true, depth: null, colors: true }));
-
-    res.status(200).send('saving tree to postgres db');
+    res.status(200).send('saving tree for user to postgres db');
+  })
+  .get((req, res) => {
+    // get all tree data for user from postgres db here
+    console.log('=> inside router .get/tree')
+    res.status(200).send('loading all saved trees for user from postgres db');
   });
 
 module.exports = router;
